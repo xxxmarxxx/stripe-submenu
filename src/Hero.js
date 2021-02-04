ea803e63ed986e699b25318342b765a8e737aead
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-04 15:32:33 +0100

    change

--- src/Hero.js
+++ src/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 const Hero = () => {
-  const { closeSubmenu } = useGlobalContext();
+  const {closeSubmenu} = useGlobalContext();
 
   return (
     <section className="hero">
