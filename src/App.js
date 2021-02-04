28230f1d42d238a4d32df8aafbaec567cbea9f3b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-04 20:39:57 +0100

    lesson 152

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Sidebar from "./Sidebar";
 import Submenu from "./Submenu";
-// lesson 146/147/148/149/150/151
+// lesson 146/147/148/149/150/151/152
 function App() {
   return (
     <>
