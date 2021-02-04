c7902c518582b1c21bddab3f60951016f4394c77
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-04 15:32:58 +0100

    add class nav rename stripe

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,15 @@ import React from "react";
 import logo from "./images/logo.svg";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
+import sublinks from "./data";
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
   return (
-    <nav>
+    <nav className="nav">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="stipe" />
+          <img src={logo} className="nav-logo" alt="stripe" />
           <button className="btn toggle-btn" onClick={openSidebar}>
             <FaBars />
           </button>
