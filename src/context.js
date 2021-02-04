13d0050d84e4197cfdbc8820e17d2dc255d6703c
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-04 12:13:32 +0100

    change lesson 146

--- src/context.js
+++ src/context.js
@@ -1,2 +1,41 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSideBarOpen,
+        openSubmenu,
+        openSideBar,
+        closeSubmenu,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
